b338045f7396da24d91723f9596fac2f793b163d
utssd <shida.shen@utexas.edu>
2017-07-03 23:54:07 -0500

    fix

--- flaskapp.py
+++ flaskapp.py
@@ -9,7 +9,6 @@ from flask import render_template
 import datetime
 
 
-from dbFunctions import *
 
 app = Flask(__name__)
 
@@ -240,6 +239,212 @@ class Labels(db.Model):
 
     def __repr__(self):
         return self.Name
+
+from sqlalchemy import func
+
+
+class dbQuery:
+    """
+    all query functions
+    """
+
+    def AllArtists(self, sorting):
+        if sorting == 'desc':
+            artists = Artists.query.order_by(
+                (db.func.lower(Artists.Name)).desc()).all()
+            for a in artists:
+                a.ArtistGenre
+            artistsInfo = [a.__dict__ for a in artists]
+        else:
+            artists = Artists.query.order_by(
+                (db.func.lower(Artists.Name)).asc()).all()
+            for a in artists:
+                a.ArtistGenre
+            artistsInfo = [a.__dict__ for a in artists]
+        return artistsInfo  # return a list of dict of artist
+
+    def ArtistByName(self, name):
+        artists = Artists.query.filter(Artists.Name == name).all()
+        for a in artists:
+            a.ArtistGenre
+        artistsInfo = [a.__dict__ for a in artists]
+        return artistsInfo  # return a list of dict of artist
+
+    def ArtistByID(self, ID):
+        artists = Artists.query.filter(Artists.ArtistID == ID).first()
+        artists.ArtistGenre
+        artistsInfo = artists.__dict__
+        return artistsInfo
+
+    def ArtistGenre(self, artist):
+        genre = Artists.query.filter(
+            Artists.Name == artist).first().ArtistGenre
+        return genre  # return a list
+
+    def ArtistByGenre(self, genre, sorting):
+        if sorting == 'desc':
+            artists = Artists.query.filter(Artists.ArtistGenre.any(Name=genre)).order_by(
+                (db.func.lower(Artists.Name)).desc()).all()
+            for a in artists:
+                a.ArtistGenre
+            artistsInfo = [a.__dict__ for a in artists]
+        else:
+            artists = Artists.query.filter(Artists.ArtistGenre.any(Name=genre)).order_by(
+                (db.func.lower(Artists.Name)).asc()).all()
+            for a in artists:
+                a.ArtistGenre
+            artistsInfo = [a.__dict__ for a in artists]
+        return artistsInfo
+
+    def ArtistSongs(self, artist):
+        songs = Songs.query.join(Artists).filter(Artists.Name == artist).all()
+        songsInfo = [s.__dict__ for s in songs]
+        return songsInfo
+
+    def ArtistImage(self, artist):
+        return Artists.query.filter(Artists.Name == artist).first().Image
+
+    def GetArtist(self, artist):
+        a = Artists.query.filter(Artists.Name == artist).first()
+        a.ArtistGenre
+        return a.__dict__
+
+    def AllSongs(self, sorting):
+        if sorting == 'desc':
+            songs = Songs.query.order_by(
+                (db.func.lower(Songs.Name)).desc()).all()
+            for s in songs:
+                s.SongGenre
+            songsInfo = [s.__dict__ for s in songs]
+        else:
+            songs = Songs.query.order_by(
+                (db.func.lower(Songs.Name)).asc()).all()
+            for s in songs:
+                s.SongGenre
+            songsInfo = [s.__dict__ for s in songs]
+        return songsInfo
+
+    def SongByID(self, ID):
+        songs = Songs.query.filter(Songs.SongID == ID).first()
+        songs.SongGenre
+        songsInfo = songs.__dict__
+        return songsInfo
+
+
+    def SongAlbum(self, song):
+        albums = Albums.query.join(Songs).filter(Songs.Name == song).all()
+        albumsInfo = [al.__dict__ for al in albums]
+        return albumsInfo
+
+    def SongByGenre(self, genre, sorting):
+        if sorting == 'desc':
+            songs = Songs.query.filter(Songs.SongGenre.any(Name=genre)).order_by(
+                (db.func.lower(Songs.Name)).desc()).all()
+            for s in songs:
+                s.SongGenre
+            songsInfo = [s.__dict__ for s in songs]
+        else:
+            songs = Songs.query.filter(Songs.SongGenre.any(Name=genre)).order_by(
+                (db.func.lower(Songs.Name)).desc()).all()
+            for s in songs:
+                s.SongGenre
+            songsInfo = [s.__dict__ for s in songs]
+        return songsInfo
+
+    def SongByAlbum(self, album):
+        songs = Songs.query.join(Albums).filter(Albums.Title == album).all()
+        for s in songs:
+            s.SongGenre
+        songsInfo = [s.__dict__ for s in songs]
+        return songsInfo
+
+    def SongImage(self, song):
+        return Songs.query.filter(Songs.Name == song).first().Image
+
+    def GetSong(self, song):
+        a = Songs.query.filter(Songs.Name == song).first()
+        a.SongGenre
+        return a.__dict__
+
+    def AllAlbums(self, sorting):
+        if sorting == 'desc':
+            albums = Albums.query.order_by(
+                (db.func.lower(Albums.Title)).desc()).all()
+            albumsInfo = [al.__dict__ for al in albums]
+        else:
+            albums = Albums.query.order_by(
+                (db.func.lower(Albums.Title)).asc()).all()
+            albumsInfo = [al.__dict__ for al in albums]
+        return albumsInfo
+
+    def AlbumByID(self, ID):
+        albums = Albums.query.filter(Albums.AlbumID == ID).first()
+        albumsInfo = albums.__dict__
+        return albumsInfo
+
+
+    def AlbumByArtist(self, artist):
+        albums = Albums.query.join(Artists).filter(
+            Artists.Name == artist).all()
+        albumsInfo = [al.__dict__ for al in albums]
+        return albumsInfo
+
+    def AlbumImage(self, album):
+        return Albums.query.filter(Albums.Title == album).first().Image
+
+    def GetAlbum(self, album):
+        a = Albums.query.filter(Albums.Title == album).first()
+        aInfo = a.__dict__
+        return aInfo
+
+    def AllGenre(self):
+        genre = Genre.query.all()
+        genreInfo = [g.__dict__ for g in genre]
+        return genreInfo
+
+    def SortArtistAsc(self):
+        artists = Artists.query.order_by(
+            (db.func.lower(Artists.Name)).asc()).all()
+        for a in artists:
+            a.ArtistGenre
+        artistsInfo = [a.__dict__ for a in artists]
+        return artistsInfo
+
+    def SortArtistDes(self):
+        artists = Artists.query.order_by(
+            (db.func.lower(Artists.Name)).desc()).all()
+        for a in artists:
+            a.ArtistGenre
+        artistsInfo = [a.__dict__ for a in artists]
+        return artistsInfo
+
+    def SortSongAsc(self):
+        songs = Songs.query.order_by((db.func.lower(Songs.Name)).asc()).all()
+        for s in songs:
+            s.SongGenre
+        songsInfo = [s.__dict__ for s in songs]
+        return songsInfo
+
+    def SortSongDes(self):
+        songs = Songs.query.order_by((db.func.lower(Songs.Name)).desc()).all()
+        for s in songs:
+            s.SongGenre
+        songsInfo = [s.__dict__ for s in songs]
+        return songsInfo
+
+    def SortAlbumAsc(self):
+        albums = Albums.query.order_by(
+            (db.func.lower(Albums.Title)).asc()).all()
+        albumsInfo = [al.__dict__ for al in albums]
+        return albumsInfo
+
+    def SortAlbumDes(self):
+        albums = Albums.query.order_by(
+            (db.func.lower(Albums.Title)).desc()).all()
+        albumsInfo = [al.__dict__ for al in albums]
+        return albumsInfo
+
+
 
 
 import flask_restless
